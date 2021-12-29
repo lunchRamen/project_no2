4fb45582b7613ed7d7c3ff0336b9a60f20e6c108
soryeongk <soryeongk.kr@gmail.com>
2021-12-30 01:38:44 +0900

    :tada: feat: poster list 완성

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,5 @@ export { default as Header } from "./common/Header";
 export { default as CommonNav } from "./common/CommonNav";
 export { default as Button } from "./common/Button";
 export { default as Table } from "./common/Table";
+export { default as Poster } from "./registerDetail/Poster";
+export { default as PosterList } from "./registerDetail/PosterList";
