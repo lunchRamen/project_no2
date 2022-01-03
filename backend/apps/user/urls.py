ae37107a51c7911756cda0eeb0e5b1b35fb1e48c
Yiseoyoon <aassdd38@naver.com>
2022-01-03 16:30:57 +0900

    user,small_theater,contents_analysis 파일 apps에 한번에 넣음

---
+++ backend/apps/user/urls.py
@@ -0,0 +1,10 @@
+# 여긴 user/urls.py
+from django.urls import path
+
+#from .views import login,register,modify,logout
+
+#app_name='user'
+
+urlpatterns=[
+    
+]
--- backend/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Prefer_ott_content_genre
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Prefer_ott_content_genre)
