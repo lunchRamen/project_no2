de052a5eced15a52dfee67d7e2ffef63156bcade
soryeongk <soryeongk.kr@gmail.com>
2022-01-07 15:47:20 +0900

    :truck: clean: del unused dir

--- backend_1/contents_analysis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContentsAnalysisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contents_analysis'
--- backend_1/small_theater/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmallTheaterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'small_theater'
