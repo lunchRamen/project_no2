e51538806f3118a73d342ded81150cb414484cc7
[3기-1반] 김민석 <bloom6561@naver.com>
2022-01-10 23:34:37 +0900

    style: remove whitespace

--- frontend/src/libs/index.js
+++ frontend/src/libs/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
-export const client = axios.create({ 
+export const client = axios.create({
   baseURL: `${baseUrl}/api`,
   headers: {
     "Content-Type": "application/json",
