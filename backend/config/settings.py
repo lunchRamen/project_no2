679c225fa92a6f4ebb36f31cc5fa4f1d58f2e7a5
[3기-1반] 김민석 <bloom6561@naver.com>
2022-01-10 23:59:46 +0900

    refactor: 설정 하드코딩 환경변수로 분리

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,7 @@ APPEND_SLASH=False
 
 # CORS
 # 1. 배포용일 경우 'google.com' , 'hostname.example.com' 등
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:8000','http://localhost:8000']
+CORS_ORIGIN_WHITELIST = [env.get("CORS_WHITELIST",default="http://localhost:8000").split(",")]
 # 2. 개발일 경우
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
--- backend/manage.py
+++ backend/manage.py
@@ -2,9 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+
+from pathlib import Path
 
 def main():
     """Run administrative tasks."""
+
+    # read .env
+    BASE_DIR = Path(__file__).resolve().parent
+    env_file = os.path.join(BASE_DIR,".env")
+
+    if os.path.exists(env_file):
+        dotenv.read_dotenv(env_file)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
